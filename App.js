1aa6806f8618f6e79e12327c82152aba847f8f8a
Khin Thiri Soe <khins@ATA-MAC04.local>
2017-10-06 18:14:52 +0700

    test

---
+++ App.js
@@ -0,0 +1,13 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+// import a library to help a Component
+import 'React' from 'react';
+import 'ReactNative' from 'react-native';
+
+// Create a Component
+
+// render it to the device
